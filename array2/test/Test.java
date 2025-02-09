d76b94e9d2b646194d97f6751ad360e3e6d8db51
Love kumar <lovekumar3009@gmail.com>
2025-02-09 20:52:03 +0530

    largest element in array

---
+++ array2/test/Test.java
@@ -0,0 +1,13 @@
+package array2.test;
+
+import java.util.Arrays;
+import java.util.*;
+public class Test {
+  
+   
+     public static void main(String[] args) {
+    int size = (int)Math.pow(10,5);
+    double[] array = new double[size];
+    System.out.println(Arrays.toString(array));
+  }
+}
