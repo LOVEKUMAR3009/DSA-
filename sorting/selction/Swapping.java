0cc3ddf3e000bc8cd67b30c2160f96dfe77287d1
Love kumar <lovekumar3009@gmail.com>
2025-02-17 21:55:43 +0530

    deleted all previous question of array

--- sorting/selction/Swapping.java
+++
@@ -1,9 +0,0 @@
-package sorting.selction;
-
-public class Swapping {
-  public static void swap(int[] arr,int firstIndex,int lastIndex){
-    int temp = arr[firstIndex];
-    arr[firstIndex] = arr[lastIndex] ;
-    arr[lastIndex] = temp ;
-  }
-}
